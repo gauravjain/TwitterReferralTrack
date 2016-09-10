9d125fd09b7b9be7c6e3e83cb5a7c94dfad383a0
Gaurav Jain <gaurav45.jain@gmail.com>
2016-09-11 02:55:14 +0530

    extracting tweet info and others

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,8 +1,14 @@
 require 'twitter'
+require 'uri'
+require 'cgi'
 
 class TweetController < ApplicationController
   def index
-    @tweets = $twitter.user_timeline('ThrillLifeBlog', count: 20)
+    @tweets = $twitter.search('@3dhubs 3dhubs.refr.cc', options = {count: 20, include_entities: true})
+  	#@referral = []
+  	#@tweets.each do |tweet|
+  	#	@uri = tweet.url[0].expanded_url
+  	#	@uri = URI(@uri).query
+  	#	@referral << Rack::Utils.parse_query @uri
   end
-  
 end
