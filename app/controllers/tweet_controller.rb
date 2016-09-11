6f2384e7cab2eee8ce1d3383c15e3407825d889b
Gaurav Jain <gaurav45.jain@gmail.com>
2016-09-12 03:08:44 +0530

    Finally fetching the Referral code with tweet info

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -4,7 +4,13 @@ require 'cgi'
 
 class TweetController < ApplicationController
   def index
+  	@base_url = "http://3dhubs.refr.cc/"
     @tweets = $twitter.search('@3dhubs 3dhubs.refr.cc', options = {count: 20, include_entities: true})
+#    @referral_code =[]
+#    @tweets.each do |tweet|
+#     uri = tweet.url[0].expanded_url
+#	  @referral_code << uri.path
+#  	end
   	#@referral = []
   	#@tweets.each do |tweet|
   	#	@uri = tweet.url[0].expanded_url
