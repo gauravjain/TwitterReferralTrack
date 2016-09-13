2b4bebf4299d6aa9f1a35e3093e0adc042f501da
Gaurav Jain <gaurav45.jain@gmail.com>
2016-09-13 09:01:25 +0530

    Code cleanup

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,20 +1,10 @@
 require 'twitter'
-require 'uri'
-require 'cgi'
 
 class TweetController < ApplicationController
   def index
-  	@base_url = "http://3dhubs.refr.cc/"
-    @tweets = $twitter.search('@3dhubs 3dhubs.refr.cc', options = {count: 20, include_entities: true})
-#    @referral_code =[]
-#    @tweets.each do |tweet|
-#     uri = tweet.url[0].expanded_url
-#	  @referral_code << uri.path
-#  	end
-  	#@referral = []
-  	#@tweets.each do |tweet|
-  	#	@uri = tweet.url[0].expanded_url
-  	#	@uri = URI(@uri).query
-  	#	@referral << Rack::Utils.parse_query @uri
+  	@client_handle = "@3dhubs"
+  	@base_url = "3dhubs.refr.cc"
+  	@search_query = @client_handle.to_s + " " + @base_url.to_s
+    @tweets = $twitter.search(@search_query, options = {count: 20, include_entities: true})
   end
 end
